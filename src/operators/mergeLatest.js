04ef93e44473bed1ced98b345894e27606261585
Cedrick Mandocdoc <cedrickmandocdoc@gmail.com>
2019-11-13 21:10:25 +0800

    Move MergeLatest to producers folder

--- src/operators/mergeLatest.js
+++ src/operators/mergeLatest.js
@@ -1,41 +1,5 @@
 import Stream from "../Stream";
-import Sink from "../Sink";
-import Merge from "../producers/Merge";
-
-class MergeLatest {
-  constructor(streams) {
-    this.streams = streams;
-  }
-
-  static join(producer, streams) {
-    return producer instanceof MergeLatest
-      ? new MergeLatest([...producer.streams, ...streams])
-      : new MergeLatest([new Stream(producer), ...streams]);
-  }
-
-  run(sink, state) {
-    const producer = new Merge(this.streams);
-
-    return producer.run(new MergeLatestSink(sink, state, this.streams), state);
-  }
-}
-
-class MergeLatestSink extends Sink {
-  constructor(sink, state, streams) {
-    super(sink, state);
-    this.seen = [];
-    this.values = [];
-    for (let index = 0; index < streams.length; index++) {
-      this.values.push(null);
-    }
-  }
-
-  next(d, i) {
-    this.values[i] = d; // mutates the current data
-    if (!this.seen[i]) this.seen[i] = true;
-    if (this.seen.length === this.values.length) this.sink.next(this.values);
-  }
-}
+import MergeLatest from "../producers/MergeLatest";
 
 const mergeLatest = streams => new Stream(new MergeLatest(streams));
 

a58d7056af21cf7e5ab16b0929138be4c235156a
Cedrick Mandocdoc <cedrickmandocdoc@gmail.com>
2019-11-19 19:41:04 +0800

    Fix lint error

--- src/operators/from.js
+++ src/operators/from.js
@@ -1,7 +1,7 @@
 import Stream from "../Stream";
 import Source from "../producers/Source";
 
-const from = data => {
+export default data => {
   const producer = new Source(sink => {
     for (let index = 0; index < data.length; index++) {
       sink.next(data[index]);
@@ -11,5 +11,3 @@ const from = data => {
 
   return new Stream(producer);
 };
-
-export default from;

045d4a9bac8f06bce06dc05f46d54e729c72fdb0
Cedrick Mandocdoc <cedrickmandocdoc@gmail.com>
2020-03-06 12:22:28 +0800

    Fix eslint error - from keyword

--- src/factories/from.js
+++ src/factories/from.js
@@ -1,14 +1,13 @@
 import create from "./create";
 
-const from = (array = []) => create(control => {
-  const open = control.open(done => {
-    done();
-    for (let index = 0; index < array.length; index++)
-      control.next(array[index]);
-    close();
+export default (array = []) =>
+  create(control => {
+    const open = control.open(done => {
+      done();
+      for (let index = 0; index < array.length; index++)
+        control.next(array[index]);
+      close();
+    });
+    const close = control.close(done => done());
+    return { open, close };
   });
-  const close = control.close(done => done());
-  return { open, close };
-});
-
-export default from;

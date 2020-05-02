7068bedabbf6ae77386769385e876bdb04c7d8d1
Cedrick Mandocdoc <cedrickmandocdoc@gmail.com>
2020-05-02 20:58:31 +0800

    Add fromObject

---
+++ src/operators/fromObject.js
@@ -0,0 +1,32 @@
+import create from "./create";
+import never from "./never";
+import { noop } from "../utils";
+import { CANCEL } from "../constants";
+
+const fromObject = object =>
+  create((open, next, fail, done, talkback) => {
+    let cancelled = false;
+    talkback.listen(
+      noop,
+      value => {
+        if (value === CANCEL) {
+          cancelled = true;
+          done(true);
+        }
+      },
+      noop,
+      noop,
+      never()
+    );
+    open();
+    const keys = Object.keys(object);
+    for (let index = 0; index < keys.length; index++) {
+      if (cancelled) break;
+      const key = keys[index];
+      const value = object[key];
+      next([value, key]);
+    }
+    done(false);
+  });
+
+export default fromObject;
---
+++ test/fromObject.test.js
@@ -0,0 +1,120 @@
+const {
+  pipe,
+  listen,
+  fromObject,
+  never,
+  teardown
+} = require("../dist/agos.cjs");
+
+describe("fromObject", () => {
+  it("should propagate each value on array", () => {
+    const received = [];
+
+    const open = jest.fn();
+    const next = jest.fn(value => received.push(value));
+    const fail = jest.fn();
+    const done = jest.fn(cancelled => expect(cancelled).toEqual(false));
+
+    pipe(
+      fromObject({ 1: 1, 2: 2, 3: 3 }),
+      listen(open, next, fail, done)
+    );
+
+    expect(open).toHaveBeenCalledTimes(1);
+    expect(next).toHaveBeenCalledTimes(3);
+    expect(fail).toHaveBeenCalledTimes(0);
+    expect(done).toHaveBeenCalledTimes(1);
+    expect(received).toEqual([[1, "1"], [2, "2"], [3, "3"]]);
+  });
+
+  it("should propagate cancellation on open", () => {
+    const received = [];
+    const abort = teardown(never());
+
+    const open = jest.fn(() => abort.run());
+    const next = jest.fn(value => received.push(value));
+    const fail = jest.fn();
+    const done = jest.fn(cancelled => expect(cancelled).toEqual(true));
+
+    pipe(
+      fromObject({ 1: 1, 2: 2, 3: 3 }),
+      listen(open, next, fail, done, abort)
+    );
+
+    expect(open).toHaveBeenCalledTimes(1);
+    expect(next).toHaveBeenCalledTimes(0);
+    expect(fail).toHaveBeenCalledTimes(0);
+    expect(done).toHaveBeenCalledTimes(1);
+    expect(received).toEqual([]);
+  });
+
+  it("should propagate cancellation on next", () => {
+    const received = [];
+    const abort = teardown(never());
+
+    const open = jest.fn();
+    const next = jest.fn(([value, key]) => {
+      received.push([value, key]);
+      if (value >= 2) abort.run();
+    });
+    const fail = jest.fn();
+    const done = jest.fn(cancelled => expect(cancelled).toEqual(true));
+
+    pipe(
+      fromObject({ 1: 1, 2: 2, 3: 3 }),
+      listen(open, next, fail, done, abort)
+    );
+
+    expect(open).toHaveBeenCalledTimes(1);
+    expect(next).toHaveBeenCalledTimes(2);
+    expect(fail).toHaveBeenCalledTimes(0);
+    expect(done).toHaveBeenCalledTimes(1);
+    expect(received).toEqual([[1, "1"], [2, "2"]]);
+  });
+
+  it("should not propagate cancellation on both before open and next", () => {
+    const received = [];
+    const abort = teardown(never());
+
+    const open = jest.fn();
+    const next = jest.fn(value => received.push(value));
+    const fail = jest.fn();
+    const done = jest.fn(cancelled => expect(cancelled).toEqual(false));
+
+    abort.run();
+
+    pipe(
+      fromObject({ 1: 1, 2: 2, 3: 3 }),
+      listen(open, next, fail, done, abort)
+    );
+
+    expect(open).toHaveBeenCalledTimes(1);
+    expect(next).toHaveBeenCalledTimes(3);
+    expect(fail).toHaveBeenCalledTimes(0);
+    expect(done).toHaveBeenCalledTimes(1);
+    expect(received).toEqual([[1, "1"], [2, "2"], [3, "3"]]);
+  });
+
+  it("should not propagate cancellation on both after open and next", () => {
+    const received = [];
+    const abort = teardown(never());
+
+    const open = jest.fn();
+    const next = jest.fn(value => received.push(value));
+    const fail = jest.fn();
+    const done = jest.fn(cancelled => expect(cancelled).toEqual(false));
+
+    pipe(
+      fromObject({ 1: 1, 2: 2, 3: 3 }),
+      listen(open, next, fail, done, abort)
+    );
+
+    abort.run();
+
+    expect(open).toHaveBeenCalledTimes(1);
+    expect(next).toHaveBeenCalledTimes(3);
+    expect(fail).toHaveBeenCalledTimes(0);
+    expect(done).toHaveBeenCalledTimes(1);
+    expect(received).toEqual([[1, "1"], [2, "2"], [3, "3"]]);
+  });
+});

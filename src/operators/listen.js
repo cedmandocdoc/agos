0109943eea76b66ad4a75ee84dda8ad9fa52b103
Cedrick Mandocdoc <cedrickmandocdoc@gmail.com>
2021-05-12 21:19:58 +0800

    Support of partial callbacks in `listen` operator

--- src/operators/listen.js
+++ src/operators/listen.js
@@ -4,7 +4,13 @@ import { noop } from "../utils";
 const listen = (sink, talkback = never()) => stream => {
   if (typeof sink === "function")
     return stream.listen(noop, sink, noop, noop, talkback);
-  return stream.listen(sink.open, sink.next, sink.fail, sink.done, talkback);
+  return stream.listen(
+    sink.open || noop,
+    sink.next || noop,
+    sink.fail || noop,
+    sink.done || noop,
+    talkback
+  );
 };
 
 export default listen;
--- test/listen.test.js
+++ test/listen.test.js
@@ -41,6 +41,24 @@ describe("listen", () => {
     expect(received).toEqual([1]);
   });
 
+  it("should accept a partial sink object", () => {
+    const received = [];
+
+    const open = jest.fn();
+    const next = jest.fn(value => received.push(value));
+    const done = jest.fn(cancelled => expect(cancelled).toEqual(false));
+
+    pipe(
+      of(1),
+      listen({ open, next, done })
+    );
+
+    expect(open).toHaveBeenCalledTimes(1);
+    expect(next).toHaveBeenCalledTimes(1);
+    expect(done).toHaveBeenCalledTimes(1);
+    expect(received).toEqual([1]);
+  });
+
   it("should accept a talkback - last parameter", () => {
     const received = [];
     const cancel = CancelInterceptor.join(never());

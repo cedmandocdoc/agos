91a75d437fdf5af0651d03f1de5c942c274eb4ae
Cedrick Mandocdoc <cedrickmandocdoc@gmail.com>
2021-05-14 11:00:30 +0800

    Permission of immediate propagation on `multicast`

--- src/operators/multicast.js
+++ src/operators/multicast.js
@@ -2,9 +2,9 @@ import create from "./create";
 import never from "./never";
 import emitter from "./emitter";
 
-const multicast = stream => {
+const multicast = (stream, options) => {
   let active = false;
-  const [controller, subject] = emitter();
+  const [controller, subject] = emitter(options);
 
   return create((open, next, fail, done, talkback) => {
     subject.listen(open, next, fail, done, talkback);
--- test/multicast.test.js
+++ test/multicast.test.js
@@ -50,6 +50,48 @@ describe("multicast", () => {
     expect(received2).toEqual([1, 2, 3]);
   });
 
+  it("should share the value immediately across all in the middle listeners", () => {
+    const received1 = [];
+    const received2 = [];
+    const source = multicast(interval(100, 3), { immediate: true });
+
+    const open1 = jest.fn();
+    const next1 = jest.fn(value => received1.push(value));
+    const fail1 = jest.fn();
+    const done1 = jest.fn(cancelled => expect(cancelled).toEqual(false));
+
+    const open2 = jest.fn();
+    const next2 = jest.fn(value => received2.push(value));
+    const fail2 = jest.fn();
+    const done2 = jest.fn(cancelled => expect(cancelled).toEqual(false));
+
+    pipe(
+      source,
+      listen({ open: open1, next: next1, fail: fail1, done: done1 })
+    );
+
+    setTimeout(() => {
+      pipe(
+        source,
+        listen({ open: open2, next: next2, fail: fail2, done: done2 })
+      );
+    }, 250);
+
+    jest.advanceTimersByTime(300);
+
+    expect(open1).toHaveBeenCalledTimes(1);
+    expect(next1).toHaveBeenCalledTimes(3);
+    expect(fail1).toHaveBeenCalledTimes(0);
+    expect(done1).toHaveBeenCalledTimes(1);
+    expect(received1).toEqual([1, 2, 3]);
+
+    expect(open2).toHaveBeenCalledTimes(1);
+    expect(next2).toHaveBeenCalledTimes(2);
+    expect(fail2).toHaveBeenCalledTimes(0);
+    expect(done2).toHaveBeenCalledTimes(1);
+    expect(received2).toEqual([2, 3]);
+  });
+
   it("should propagate cancellation", () => {
     const received1 = [];
     const received2 = [];

05ca25e603e27c5c482edfcc59cdb6b3ec54bf21
Cedrick Mandocdoc <cedrickmandocdoc@gmail.com>
2021-05-17 13:03:18 +0800

    Support of object streams in `mergeLatest`

--- src/operators/mergeLatest.js
+++ src/operators/mergeLatest.js
@@ -3,14 +3,24 @@ import scan from "./scan";
 import filter from "./filter";
 import { pipe } from "../utils";
 
-const mergeLatest = streams =>
-  pipe(
+const mergeLatest = streams => {
+  const isArray = streams instanceof Array;
+  return pipe(
     merge(streams, true),
-    scan((values, [value, index]) => {
-      values[index] = value;
-      return values;
-    }, []),
-    filter(values => values.length >= streams.length)
+    scan(
+      (values, [value, index]) => {
+        const copy = isArray ? [...values] : { ...values };
+        copy[index] = value;
+        return copy;
+      },
+      isArray ? [] : {}
+    ),
+    filter(values =>
+      isArray
+        ? values.length >= streams.length
+        : Object.keys(values).length >= Object.keys(streams).length
+    )
   );
+};
 
 export default mergeLatest;
--- test/mergeLatest.test.js
+++ test/mergeLatest.test.js
@@ -4,7 +4,7 @@ const { pipe, listen, mergeLatest, map } = require("../dist/agos.cjs");
 jest.useFakeTimers();
 
 describe("mergeLatest", () => {
-  it("should propagate all latest values from sources", () => {
+  it("should propagate all latest values from array sources", () => {
     const expected = [[2, 1], [3, 1], [3, 2]];
 
     const open = jest.fn();
@@ -18,6 +18,34 @@ describe("mergeLatest", () => {
     );
 
     jest.advanceTimersByTime(400);
+
+    expect(open).toHaveBeenCalledTimes(1);
+    expect(next).toHaveBeenCalledTimes(3);
+    expect(fail).toHaveBeenCalledTimes(0);
+    expect(done).toHaveBeenCalledTimes(1);
+  });
+
+  it("should propagate all latest values from object sources", () => {
+    const expected = [
+      { "0": 2, "1": 1 },
+      { "0": 3, "1": 1 },
+      { "0": 3, "1": 2 }
+    ];
+
+    const open = jest.fn();
+    const next = jest.fn(value => expect(value).toEqual(expected.shift()));
+    const fail = jest.fn();
+    const done = jest.fn(cancelled => expect(cancelled).toEqual(false));
+
+    pipe(
+      mergeLatest({
+        0: interval(100, 3),
+        1: interval(200, 2)
+      }),
+      listen({ open, next, fail, done })
+    );
+
+    jest.advanceTimersByTime(4000);
 
     expect(open).toHaveBeenCalledTimes(1);
     expect(next).toHaveBeenCalledTimes(3);
